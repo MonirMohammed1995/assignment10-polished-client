cde97be15493ea61d7d41c5c8892ec499104cb36
MonirMohammed1995 <monirmdnayemsaju42@gmail.com>
2025-05-24 00:21:20 +0600

    Hover and tooltip install

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 const PlantCard = ({ plant }) => {
     const {_id,image,name,category,careLevel,healthStatus}=plant;
   return (
-    <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 flex flex-col sm:flex-row overflow-hidden">
+    <div className="flex flex-col overflow-hidden transition-transform duration-300 -translate-y-1 bg-white shadow-md rounded-2xl hover:shadow-xl hover: sm:flex-row">
       <img
         src={image}
         alt={name}
-        className="w-full sm:w-48 h-48 object-cover"
+        className="object-cover w-full h-48 sm:w-48"
       />
-      <div className="flex-1 p-4 flex flex-col justify-between">
+      <div className="flex flex-col justify-between flex-1 p-4">
         <div>
           <h3 className="text-2xl font-bold text-green-800">{name}</h3>
-          <p className="text-gray-600 mt-1">
+          <p className="mt-1 text-gray-600">
             <span className="font-medium">Category:</span> {category}
           </p>
-          <p className="text-gray-600 mt-1">
+          <p className="mt-1 text-gray-600">
             <span className="font-medium">Care Level:</span> {careLevel}
           </p>
-          <p className="text-gray-600 mt-1">
+          <p className="mt-1 text-gray-600">
             <span className="font-medium">Health Status:</span>{" "}
             {healthStatus}
           </p>
         </div>
         <div className="mt-4">
           <Link to={`/plants/${_id}`}>
-          <button className="bg-green-600 text-white px-5 py-2 rounded-full hover:bg-green-700 transition">
+          <button className="px-5 py-2 text-white transition bg-green-600 rounded-full hover:bg-green-700">
              View Details
           </button>
           </Link>
